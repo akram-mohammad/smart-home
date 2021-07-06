d70d5580c12b042011cd312cdf8e312124cb9dab
akram-mohammad <akram.mohamed.dev@gmail.com>
2021-07-06 16:54:47 +0200

    forgot password page created

--- lib/main.dart
+++ lib/main.dart
@@ -31,25 +31,38 @@ class MyApp extends StatelessWidget {
         // Define the default TextTheme. Use this to specify the default
         // text styling for headlines, titles, bodies of text, and more.
         textTheme: TextTheme(
-          headline1: TextStyle(
-              fontSize: 36.0, fontWeight: FontWeight.w700, color: Colors.white),
-          headline2: TextStyle(
-              fontSize: 36.0, fontWeight: FontWeight.w700, color: Colors.white),
-          headline3: TextStyle(
-              fontSize: 32.0, fontWeight: FontWeight.w700, color: Colors.white),
-          headline4: TextStyle(
-              fontSize: 72.0, fontWeight: FontWeight.w700, color: Colors.white),
-          headline5: TextStyle(
-            fontSize: 14.0,
-            fontWeight: FontWeight.w600,
-            color: Color(0xFF1FD38F),
-          ),
-          headline6: TextStyle(
-              fontSize: 14.0, fontWeight: FontWeight.w400, color: Colors.white),
-          bodyText1:
-              TextStyle(fontSize: 14.0, color: Colors.white, height: 1.6),
-          bodyText2: TextStyle(fontSize: 14.0, color: Colors.white),
-        ),
+            headline1: TextStyle(
+                fontSize: 36.0,
+                fontWeight: FontWeight.w700,
+                color: Colors.white),
+            headline2: TextStyle(
+                fontSize: 36.0,
+                fontWeight: FontWeight.w700,
+                color: Colors.white),
+            headline3: TextStyle(
+                fontSize: 32.0,
+                fontWeight: FontWeight.w700,
+                color: Colors.white),
+            headline4: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.w700,
+                color: Colors.white),
+            headline5: TextStyle(
+              fontSize: 14.0,
+              fontWeight: FontWeight.w600,
+              color: Color(0xFF1FD38F),
+            ),
+            headline6: TextStyle(
+                fontSize: 14.0,
+                fontWeight: FontWeight.w400,
+                color: Colors.white),
+            bodyText1:
+                TextStyle(fontSize: 14.0, color: Colors.white, height: 1.6),
+            bodyText2: TextStyle(fontSize: 14.0, color: Colors.white),
+            caption: TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.w600,
+                color: Colors.white)),
       ),
       home: MyHomePage(title: 'Smart Home'),
     );
--- lib/ui/screens/authentication/forgot_pass.dart
+++ lib/ui/screens/authentication/forgot_pass.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:smart_home/ui/widgets/empty_appbar.dart';
+import 'package:smart_home/ui/widgets/regular_elevated_button.dart';
+import 'package:smart_home/ui/widgets/regular_text_field.dart';
 
 class ForgotPassword extends StatelessWidget {
   const ForgotPassword({Key? key}) : super(key: key);
@@ -10,22 +12,68 @@ class ForgotPassword extends StatelessWidget {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       appBar: EmptyAppbar(),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Forgot Password',
-              style: Theme.of(context).textTheme.headline3,
-            ),
-            Center(
-              child: SvgPicture.asset(
-                'assets/imgs/forgot_password.svg',
-                width: 300.0,
-                height: 300.0,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Forgot Password?',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                  SizedBox(
+                    height: 2.0,
+                  ),
+                  Text(
+                    'No worries, you can reset it',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                ],
               ),
-            ),
-          ],
+              SizedBox(
+                height: 25.0,
+              ),
+              Center(
+                child: SvgPicture.asset(
+                  'assets/imgs/forgot_password.svg',
+                  width: 250.0,
+                  height: 250.0,
+                ),
+              ),
+              SizedBox(
+                height: 25.0,
+              ),
+              Center(
+                child: Text(
+                  'Enter the email address \nassociated with your account',
+                  style: Theme.of(context).textTheme.caption,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Center(
+                child: Text(
+                  'We will email you a verification code \nto reset your password',
+                  style: Theme.of(context).textTheme.bodyText1,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 25.0,
+              ),
+              RegularTextField(),
+              SizedBox(
+                height: 10.0,
+              ),
+              RegularElevatedButton(title: 'Send OTP')
+            ],
+          ),
         ),
       ),
     );
